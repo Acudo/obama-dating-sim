4d6104d3307332cc8969a5ce58e2437ff550f4bc
Acudo <44158543+Acudo@users.noreply.github.com>
2021-05-10 13:00:17 +1000

    Update obama-dating-sim.py

--- obama-dating-sim.py
+++ obama-dating-sim.py
@@ -1,1 +1,22 @@
+# Modules
+import time
 
+# Definitions
+def name():
+    n = input("First of all, what is your name? ")
+    yn = input(f"{n}, is that correct? (y/n) ")
+    if yn == 'y':
+        print(f"Hopyfully your name will become {n} Obama, haha")
+    elif yn == 'n':
+        name()
+    else:
+        print("I don't quite understand. I'll assume it's a yes")
+    return n
+
+# The Game
+
+# Start
+print("Welcome to the Obama Dating Simulator")
+
+# Asking for name
+name = name()
